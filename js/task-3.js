f2266882e474714341dc3787bc872449775d86d3
Mila-IT <133154151+Mila-IT@users.noreply.github.com>
2024-11-13 21:47:41 +0200

    add options task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,37 @@
 function filterArray(numbers, value) {
-    let newArray = []
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            newArray.push(numbers[i])
+    const newArray = [];
+    for (const number of numbers) {
+        if (number > value) {
+            newArray.push(number);
         }
     }
-    return newArray
+    return newArray;
 }
+
+// Function declaration
+
+// function filterArray(numbers, value) {
+//     let newArray = []
+//     for (let i = 0; i < numbers.length; i += 1){
+//         if (numbers[i] > value) {
+//             newArray.push(numbers[i])
+//         }
+//     }
+//     return newArray
+// }
+
+
+// Function expression
+
+// const filterArray = function(numbers, value) {
+//     let newArray = []
+//     for (let i = 0; i < numbers.length; i += 1){
+//         if (numbers[i] > value) {
+//             newArray.push(numbers[i])
+//         }
+//     }
+//     return newArray
+// }
 
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
